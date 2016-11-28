11e3624641f7ef87ffaa3b9fc561659c5da25708
mrjadaml <mrjadaml@gmail.com>
2016-11-28 13:17:15 -0800

    Hello wurld 🐣

---
+++ Brewfile
@@ -0,0 +1,50 @@
+cask_args appdir: '/Applications'
+
+# taps are Formulaes/commands pulled from a Git repo
+tap 'caskroom/cask'
+tap 'caskroom/versions'
+tap 'homebrew/services'
+
+# brew is used for installing MacOS dependencies/binaries
+brew 'ctags'
+brew 'bash'
+brew 'git'
+brew 'git-secrets'
+brew 'heroku-toolbelt'
+brew 'httpie'
+brew 'imagemagick'
+brew 'mas'
+brew 'openssl'
+brew 'phantomjs'
+brew 'postgresql'
+brew 'reattach-to-user-namespace'
+brew 'redis'
+brew 'tmux'
+brew 'vim'
+brew 'wget'
+brew 'zsh'
+
+# cask is used for installing Mac applications
+cask 'atom'
+cask 'alfred'
+cask 'cloud'
+cask 'dash'
+cask 'dropbox'
+cask 'firefox'
+cask 'flux'
+cask 'github-desktop'
+cask 'google-chrome'
+cask 'google-chrome-canary'
+cask 'iterm2'
+cask 'slack'
+
+# mas is used for installing Mac App Store applications
+# aka, applications not yet provided through cask
+
+# optional sign in to app store
+# you can provide pw here, or be prompted for it durring run time
+# mas signin example@mail.com "som3Rand0p@sswOrd'
+
+# mas 'Pixelmator', id: 407963104
+# mas 'Sip', id: 507257563
+# mas 'Xcode', id: 497799835
---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# import helper functions
+source ./helpers/main
+
+# Welcome to the laptop script!
+
+trap 'ret=$?; test $ret -ne 0 && printf "failed\n\n" >&2; exit $ret' EXIT
+
+set -e
+
+# Make ~/.bin if it does not exsit
+if [ ! -d "$HOME/.bin/" ]; then
+  mkdir "$HOME/.bin"
+fi
+
+source ./run/zsh/setup
+
+# Install brew if not already installed
+if ! command -v brew >/dev/null; then
+  fancy_echo "Installing Homebrew ..."
+    curl -fsS \
+      'https://raw.githubusercontent.com/Homebrew/install/master/install' | ruby
+
+    append_to_file "$HOME/.zshrc" 'export PATH="/usr/local/bin:$PATH"'
+fi
+
+fancy_echo "Updating Homebrew formulas ..."
+brew update --verbose
+
+fancy_echo "Brew doctor is verifying the Homebrew installation 🚑..."
+if brew doctor; then
+  fancy_echo "Your Homebrew installation is good to go."
+else
+  fancy_echo "Your Homebrew installation reported some errors or warnings."
+  echo "If the warnings are related to Python, you can ignore them."
+  echo "Otherwise, review the Homebrew messages to see if any action is needed."
+fi
+
+fancy_echo "Installing formulas and casks from the Brewfile 🍺..."
+brew bundle
+
+source ./run/node/setup
+source ./run/python/setup
+source ./run/ruby/setup
+
+source ./run/osx/setup
+
+fancy_echo 'All done!'
