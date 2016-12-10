6442cacaaef81a9494e8937cba839373889b2259
mrjadaml <mrjadaml@gmail.com>
2016-12-10 00:19:56 -0800

    moved brew setup into homebrew module 🍺

--- install.sh
+++ install.sh
@@ -16,36 +16,7 @@ fi
 
 source ./run/zsh/setup
 
-# Install brew if not already installed
-if ! command -v brew >/dev/null; then
-  fancy_echo "Installing Homebrew ..."
-    curl -fsS \
-      'https://raw.githubusercontent.com/Homebrew/install/master/install' | ruby
-
-    append_to_file "$HOME/.zshrc" 'export PATH="/usr/local/bin:$PATH"'
-fi
-
-fancy_echo "Updating Homebrew formulas ..."
-brew update --verbose
-
-fancy_echo "Brew doctor is verifying the Homebrew installation 🚑..."
-if brew doctor; then
-  fancy_echo "Your Homebrew installation is good to go."
-else
-  fancy_echo "Your Homebrew installation reported some errors or warnings."
-  echo "If the warnings are related to Python, you can ignore them."
-  echo "Otherwise, review the Homebrew messages to see if any action is needed."
-fi
-
-fancy_echo "Installing formulas and casks from the Brewfile 🍺..."
-brew bundle --file=./run/homebrew/Brewfile
-
-echo "Installing cask apps..."
-brew bundle --file=./run/homebrew/brew-casks
-
-echo "Installing Mas apps, including Xcode. This may take a little while..."
-brew bundle --file=./run/homebrew/brew-mas
-
+source ./run/homebrew/setup
 
 source ./run/node/setup
 source ./run/python/setup
